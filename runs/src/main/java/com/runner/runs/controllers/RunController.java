532cf90623cb17cfa69c7145fd904097addd43dc
pouliose <vaggpouli@gmail.com>
2024-12-24 15:58:51 +0200

    updated with kafka producer

--- runs/src/main/java/com/runner/runs/controllers/RunController.java
+++ runs/src/main/java/com/runner/runs/controllers/RunController.java
@@ -1,24 +1,30 @@
 package com.runner.runs.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.runner.runs.domain.Run;
 import com.runner.runs.repositories.RunRepository;
 import com.runner.runs.exceptions.RunNotFoundException;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import com.runner.runs.service.RunKafkaProducerService;
 
 import java.util.List;
 import java.util.Optional;
 
 @RequestMapping("api/v1/runs")
 @RestController
+@AllArgsConstructor
 public class RunController {
 
-    private final RunRepository runRepository;
+    private static final Logger LOG = LoggerFactory.getLogger(RunController.class);
 
-    public RunController(RunRepository runRepository) {
-        this.runRepository = runRepository;
-    }
+
+    private final RunRepository runRepository;
+    private final RunKafkaProducerService runKafkaProducerService;
 
     @GetMapping("")
     List<Run> findAll() {
@@ -36,8 +42,12 @@ public class RunController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    void create(@Valid @RequestBody Run run){
-        runRepository.save(run);
+    void create(@Valid @RequestBody Run run) throws JsonProcessingException {
+
+        Run savedRun = runRepository.save(run);
+        LOG.debug("Run saved: {}", savedRun);
+
+        runKafkaProducerService.sendRunToKafka(savedRun);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
