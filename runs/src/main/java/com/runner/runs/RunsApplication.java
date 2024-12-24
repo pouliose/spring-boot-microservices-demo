cff847a2dba3b8bdaade2444acc66ccc3bf4aae8
pouliose <vaggpouli@gmail.com>
2024-12-24 19:17:17 +0200

    added consumer app Statistics

--- runs/src/main/java/com/runner/runs/RunsApplication.java
+++ runs/src/main/java/com/runner/runs/RunsApplication.java
@@ -1,25 +1,13 @@
 package com.runner.runs;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
-import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class RunsApplication {
-	private static final Logger LOG = LoggerFactory.getLogger(RunsApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunsApplication.class, args);
 	}
-
-	/*@KafkaListener(topics = "run.upload")
-	@Profile("!test")
-	public String listens(final String in) {
-		LOG.debug("In payload {}", in);
-		return in;
-	}*/
 
 }
