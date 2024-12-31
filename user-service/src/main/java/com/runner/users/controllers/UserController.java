86d05d405d1b4e9bd84f30e8d545ac0cf2febae8
pouliose <vaggpouli@gmail.com>
2025-01-01 00:01:04 +0200

    user-service: use of PagingAndSortingRepository, added openapi

--- user-service/src/main/java/com/runner/users/controllers/UserController.java
+++ user-service/src/main/java/com/runner/users/controllers/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RequestMapping("api/v1/users")
