dda4580c1f8bbc4d2f6551b81fda3ecb2792499f
pouliose <vaggpouli@gmail.com>
2025-01-20 23:33:11 +0200

    WebMvcConfigurer used in the beginning. Now we need to use the default restClient builder that spring boot auto-configured for use that can propagate the traceId to user-service

--- run-service/src/main/java/com/runner/runs/config/WebConfig.java
+++ run-service/src/main/java/com/runner/runs/config/WebConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+   /* @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
@@ -20,5 +20,5 @@ public class WebConfig implements WebMvcConfigurer {
                         .allowCredentials(true);
             }
         };
-    }
+    }*/
 }
